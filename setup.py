2b69d297e57bf60e7208e4f82ce1916a94ec297f
Arnon Yaari <arnony@infinidat.com>
2015-10-21 18:13:47 +0300

    bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open('test-requirements.txt') as f:
 
 setup(
    name='pyvmomi',
-   version='5.5.0.2',
+   version='5.5.0.3',
    description='VMware vSphere Python SDK',
    # NOTE: pypi prefers the use of RST to render docs
    long_description=read('README.rst'),
